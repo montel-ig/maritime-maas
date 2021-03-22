a86530d7652ef01112fae2a3be33d00000e7b7fe
Tuomas Haapala <tuomas.haapala@anders.fi>
2021-03-22 03:18:21 +0200

    Add Fare and FareRule to admin UI

--- gtfs/admin.py
+++ gtfs/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 
-from .models import Agency, Calendar, CalendarDate, Feed, Route, Stop, StopTime, Trip
+from .models import (
+    Agency,
+    Calendar,
+    CalendarDate,
+    Fare,
+    FareRule,
+    Feed,
+    Route,
+    Stop,
+    StopTime,
+    Trip,
+)
 
 admin.site.register(Agency)
 admin.site.register(Calendar)
+admin.site.register(Fare)
+admin.site.register(FareRule)
 admin.site.register(CalendarDate)
 admin.site.register(Feed)
 admin.site.register(Route)
