625581f107118142a41eb5d2f818766b8d1db932
Juha Louhiranta <juha.louhiranta@anders.fi>
2021-03-29 23:47:56 +0300

    Add ticketing system model to link feeds to transport service providers
    Refs MAAS-43

--- gtfs/models/feed.py
+++ gtfs/models/feed.py
@@ -6,10 +6,18 @@ from .base import TimeStampedModel
 
 class Feed(TimeStampedModel):
     name = models.CharField(verbose_name=_("name"), max_length=255)
+    ticketing_system = models.ForeignKey(
+        "maas.TicketingSystem",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        verbose_name=_("ticketing system"),
+    )
 
     class Meta:
         verbose_name = _("feed")
         verbose_name_plural = _("feeds")
+        default_related_name = "feeds"
 
     def __str__(self):
         return self.name
