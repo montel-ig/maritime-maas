f64a0a41b2d09e2e2f8cf92f1d7123547cdddd85
Santtu Tuovinen <santtu.tuovinen@hel.fi>
2021-03-31 11:48:37 +0300

    Create axios instance.

--- frontend/src/domain/hooks/api-hooks.ts
+++ frontend/src/domain/hooks/api-hooks.ts
@@ -1,14 +1,15 @@
 import { useQuery } from 'react-query';
-import axios from 'axios';
+
+import { axiosInstance } from '../../common/util/axios';
 
 export const useStops = (params: string) => {
   return useQuery('stops', () =>
-    axios.get(`/v1/stops/?${params}`).then((response) => response.data)
+    axiosInstance.get(`/v1/stops/?${params}`).then((response) => response.data)
   );
 };
 
 export const useRoutes = (params: string) => {
   return useQuery('routes', () =>
-    axios.get(`/v1/routes/?${params}`).then((response) => response.data)
+    axiosInstance.get(`/v1/routes/?${params}`).then((response) => response.data)
   );
 };
