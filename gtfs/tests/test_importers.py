566f007e4fc77f2cf0ff3daada0e3124b89deb23
Juha Louhiranta <juha.louhiranta@anders.fi>
2021-04-09 00:52:36 +0300

    Import rider_categories and fare_rider_categories from a GTFS feed
    Refs MAAS-56

--- gtfs/tests/test_importers.py
+++ gtfs/tests/test_importers.py
@@ -1,9 +1,10 @@
 import datetime
+from decimal import Decimal
 
 import pytest
 
 from gtfs.importers import GTFSFeedImporter
-from gtfs.models import Agency
+from gtfs.models import Agency, Fare, RiderCategory
 
 
 @pytest.mark.django_db
@@ -36,3 +37,23 @@ def test_gtfs_feed_importer():
     stop = stop_time.stop
     assert stop.name == "Kauppatori"
     assert stop.point
+
+    assert route.fare_rules.count() == 1
+    fare_rule = route.fare_rules.first()
+
+    fare = fare_rule.fare
+    assert fare.price == Decimal("5.5")
+    assert fare.currency_type == "EUR"
+    assert fare.payment_method == Fare.PaymentMethod.BEFORE_BOARDING
+    assert fare.transfers == Fare.Transfers.TWO_TRANSFERS
+
+    assert fare.fare_rider_categories.count() == 3
+    fare_rider_category = fare.fare_rider_categories.first()
+    assert fare_rider_category.price == Decimal("4.5")
+    assert fare_rider_category.currency_type == "EUR"
+
+    assert RiderCategory.objects.count() == 3
+    rider_category = RiderCategory.objects.first()
+    assert rider_category.source_id == "pensioner"
+    assert rider_category.name == "Eläkeläinen"
+    assert rider_category.description == "Eläkeläinen"
